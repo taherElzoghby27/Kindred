f76aac93d386b4fd38e7c46f987773c491d06cb6
taherElzoghby27 <tataamen678@gmail.com>
2025-08-16 01:40:44 +0300

    added constraint for account

--- src/main/java/com/spring/boot/social/dto/AccountDto.java
+++ src/main/java/com/spring/boot/social/dto/AccountDto.java
@@ -1,7 +1,10 @@
 package com.spring.boot.social.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,8 +19,13 @@ public class AccountDto {
     private Long id;
     private String username;
     @NotEmpty(message = "empty.email")
+    @Email(message = "email.format")
     private String email;
-    @NotEmpty(message = "empty.password")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).{7,}$",
+            message = "error.password"
+    )
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private Long enabled;
     private String firstName;
--- src/main/java/com/spring/boot/social/mappers/AccountMapper.java
+++ src/main/java/com/spring/boot/social/mappers/AccountMapper.java
@@ -3,14 +3,12 @@ package com.spring.boot.social.mappers;
 import com.spring.boot.social.dto.AccountDto;
 import com.spring.boot.social.models.security.Account;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface AccountMapper {
     AccountMapper ACCOUNT_MAPPER = Mappers.getMapper(AccountMapper.class);
 
-    @Mapping(target = "password", ignore = true)
     AccountDto toAccountDto(Account account);
 
     Account toAccount(AccountDto accountDto);
