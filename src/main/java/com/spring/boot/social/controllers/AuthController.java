75ac7dc90af457895e5d9c9fa439d8ab1c65970c
taherElzoghby27 <tataamen678@gmail.com>
2025-08-18 00:37:05 +0300

    update account controller

--- src/main/java/com/spring/boot/social/controllers/AuthController.java
+++ src/main/java/com/spring/boot/social/controllers/AuthController.java
@@ -1,7 +1,32 @@
 package com.spring.boot.social.controllers;
 
+
+import com.spring.boot.social.dto.AccountDto;
+import com.spring.boot.social.services.AuthService;
+import com.spring.boot.social.vm.AccountResponseVm;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
+@RequestMapping("/auth")
 @RestController
 public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<AccountResponseVm> signUp(@Valid @RequestBody AccountDto accountDto) {
+        return ResponseEntity.created(URI.create("/sign-up")).body(authService.signup(accountDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AccountResponseVm> login(@Valid @RequestBody AccountDto accountDto) {
+        return ResponseEntity.ok(authService.login(accountDto));
+    }
 }
