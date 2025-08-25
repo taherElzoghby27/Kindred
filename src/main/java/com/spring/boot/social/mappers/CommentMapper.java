ea10b1ad14035cf862aa12b2cc128cc312bea22b
taherElzoghby27 <tataamen678@gmail.com>
2025-08-25 21:58:35 +0300

    make mapper for comment

---
+++ src/main/java/com/spring/boot/social/mappers/CommentMapper.java
@@ -0,0 +1,18 @@
+package com.spring.boot.social.mappers;
+
+import com.spring.boot.social.dto.CommentDto;
+import com.spring.boot.social.models.Comment;
+import com.spring.boot.social.vm.CommentRequestVm;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CommentMapper {
+    CommentMapper COMMENT_MAPPER = Mappers.getMapper(CommentMapper.class);
+
+    CommentDto toCommentDto(Comment comment);
+
+    @Mapping(source = "postId", target = "post", ignore = true)
+    Comment toComment(CommentRequestVm commentRequestVm);
+}
