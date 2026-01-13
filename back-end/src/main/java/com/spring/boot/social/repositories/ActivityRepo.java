e8be965ada71156fea35c7aa8646a11912ec2216
taherElzoghby27 <tataamen678@gmail.com>
2026-01-14 00:24:52 +0200

    update controllers

--- back-end/src/main/java/com/spring/boot/social/repositories/ActivityRepo.java
+++ back-end/src/main/java/com/spring/boot/social/repositories/ActivityRepo.java
@@ -4,7 +4,9 @@ import com.spring.boot.social.entity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActivityRepo extends JpaRepository<Activity, Long> {
-
+    List<Activity> findActivitiesByAccountId(Long accountId);
 }
