50b3caa62fe22e9429c8a14ace07608a034b59ce
taherElzoghby27 <tataamen678@gmail.com>
2026-01-19 08:07:07 +0200

    impl save notification

--- back-end/src/main/java/com/spring/boot/social/services/impl/NotificationServiceImpl.java
+++ back-end/src/main/java/com/spring/boot/social/services/impl/NotificationServiceImpl.java
@@ -1,7 +1,11 @@
 package com.spring.boot.social.services.impl;
 
 import com.spring.boot.social.dto.NotificationDto;
+import com.spring.boot.social.entity.Account;
+import com.spring.boot.social.entity.Notification;
+import com.spring.boot.social.mappers.NotificationMapper;
 import com.spring.boot.social.repositories.NotificationRepo;
+import com.spring.boot.social.services.AccountService;
 import com.spring.boot.social.services.NotificationService;
 import com.spring.boot.social.vm.NotificationRequestVm;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +17,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
     private final NotificationRepo notificationRepo;
+    private final AccountService accountService;
 
     @Override
     public void saveNotification(NotificationRequestVm notificationRequestVm) {
-
+        Notification notification = NotificationMapper.INSTANCE.toNotification(notificationRequestVm);
+        notification.setRead(false);
+        //get recipient by id
+        Account recipient = accountService.getAccount(notificationRequestVm.getRecipientId());
+        notification.setRecipient(recipient);
+        //save notification in db
+        notificationRepo.save(notification);
     }
 
     @Override
